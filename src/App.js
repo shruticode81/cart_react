0a66f7b95978d89fc6eebeff7b6a4e4d3aa493b8
shruti anand <vitikaawesome81@gmail.com>
2021-02-20 18:29:00 +0530

    added cartItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
-
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
+      {/* <h1>Cart</h1> */}
+      <CartItem/>
+
     </div>
   );
 }
